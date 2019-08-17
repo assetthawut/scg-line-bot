791e5e294fa563a0072c9b25333b1c124c4904a4
Setthawut <assetthawut@gmail.com>
2019-08-17 14:57:07 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
     require "vendor/autoload.php";
     $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('cUxb83lhQFSLy4Ezs/im8hOUp7ci1CIBMZvj+5VDck4fJ8onlzMloqM95nKDs1Hp4LGj3ujlvOyFmFzeFb+/nEPOlSbHgnELtszqBFCaYcs9cMw2m+jDHh6Nj74jZsYheXQPHe3GRo8n3TCSv4OAugdB04t89/1O/w1cDnyilFU=');
     $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => 'baacaa0d19f543b23583afd2059b9a74']);
+    $body 	   = file_get_contents('php://input');
     $data = json_decode($body, true);
     print_r($data);
 
